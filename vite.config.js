3df8d9c806581f9ad1c8cf6d094f4721423ca2a2
Faisu <faisal.khan786hi@gmail.com>
2024-12-04 17:18:26 +0530

    seconds commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/bhumi_website/', // Replace with your GitHub repository name
+});
